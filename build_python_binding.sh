88e79e8615a54f8d26b9056958105f2d6572a02c
corenel <xxdsox@gmail.com>
2018-02-28 21:12:24 +0800

    generate python binding successfully

---
+++ build_python_binding.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+echo "generate wrapper by swig..."
+swig -I../opencv-swig/lib -I/usr/local/include -python -c++ wrapper/plate_detector.i
+echo "generate shared library by g++..."
+g++ -shared -fpic wrapper/plate_detector_wrap.cxx \
+  -Iinclude -Isrc -Iwrapper \
+  -Lbuild -Lbuild/thirdparty -leasypr -lthirdparty \
+  $(pkg-config --cflags --libs python3) $(pkg-config --libs opencv) \
+  --std=c++11 \
+  -o wrapper/_plate_detector.so
+echo "copy libeasypr and libthirdparty..."
+cp build/libeasypr.so wrapper
+cp build/thirdparty/libthirdparty.so wrapper
