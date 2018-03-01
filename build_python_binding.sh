babab3466fde294572bca1dcf10f2ba50b450830
corenel <xxdsox@gmail.com>
2018-03-01 13:09:14 +0800

    add setup for python package

--- build_python_binding.sh
+++ build_python_binding.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 echo "generate wrapper by swig..."
 swig -I../opencv-swig/lib -I/usr/local/include -python -c++ wrapper/plate_detector.i
+
 echo "generate shared library by g++..."
 g++ -shared -fpic wrapper/plate_detector_wrap.cxx \
   -Iinclude -Isrc -Iwrapper \
@@ -8,8 +9,13 @@ g++ -shared -fpic wrapper/plate_detector_wrap.cxx \
   $(pkg-config --cflags --libs python3) $(pkg-config --libs opencv) \
   --std=c++11 \
   -o wrapper/_plate_detector.so
-echo "copy libeasypr and libthirdparty..."
-cp build/libeasypr.so wrapper
-cp build/thirdparty/libthirdparty.so wrapper
+
+echo "add libeasypr and libthirdparty to path..."
+export LD_LIBRARY_PATH=$(pwd):$LD_LIBRARY_PATH 
+echo "export LD_LIBRARY_PATH=$(pwd):$LD_LIBRARY_PATH" >> $HOME/.zshrc  # or .bashrc
+
 echo "link model..."
 ln -s ../model/ wrapper
+
+echo "install python package..." 
+pip install -e wrapper
