ab4970fbd3d1669d1fdd1a7686b65f79888c72c4
corenel <xxdsox@gmail.com>
2018-03-02 16:59:35 +0800

    log elapsed time for detection.

--- wrapper/wrapper.cpp
+++ wrapper/wrapper.cpp
@@ -1,4 +1,5 @@
 #include "wrapper.h"
+#include <chrono>
 
 
 int main(int argc, const char *argv[]) {
@@ -8,8 +9,13 @@ int main(int argc, const char *argv[]) {
 
     std::string filename = argv[1];
     cv::Mat src = imread(filename);
+    std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
     std::string license = Recognize(src);
+    std::chrono::steady_clock::time_point end= std::chrono::steady_clock::now();
     std::cout << license << std::endl;
+    std::cout << "Duration = " << std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count() << "ms" <<std::endl;
+    std::cout << "Duration = " << std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count() << "us" <<std::endl;
+    std::cout << "Duration = " << std::chrono::duration_cast<std::chrono::nanoseconds> (end - begin).count() << "ns" <<std::endl;
     return 0;
 }
 
